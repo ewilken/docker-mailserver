e3c3d928cb3246f203f9a9be541343bfd6849c7f
Nick Pappas <nick@radicand.org>
2019-10-27 08:32:49 -0500

    add latest tag

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -4,6 +4,6 @@
 VCS_REF=$(shell git rev-parse --short HEAD)
 VCS_VERSION=$(shell git describe)
 
-docker buildx build --platform linux/arm/v7	--build-arg VCS_REF=$(VCS_REF) --build-arg VCS_VERSION=$(VCS_VERSION) -f Dockerfile -t radicand/docker-mailserver:`git rev-parse --verify --short HEAD` .
+docker buildx build --platform linux/arm/v7	--build-arg VCS_REF=$(VCS_REF) --build-arg VCS_VERSION=$(VCS_VERSION) -f Dockerfile -t radicand/docker-mailserver:`git rev-parse --verify --short HEAD` -t radicand/docker-mailserver:latest .
 
 #docker push radicand/docker-mailserver
